8d67fb654129d2c82cf44b57e1d4b9414f93c8df
Edward Tubbs <ed@silesiacoin.org>
2018-10-19 18:21:47 +0200

    Added validation of user name, refactored js code, added red borded when user name is invalid in input and when user name is not found.

--- src/app.js
+++ src/app.js
@@ -1,29 +1,68 @@
 import './assets/scss/app.scss';
 import $ from 'cash-dom';
 
-
 export class App {
   initializeApp() {
-    let self = this;
+      let self = this;
+      const input = $('.username.input');
 
-    $('.load-username').on('click', function (e) {
-      let userName = $('.username.input').val();
+      setDefaults();
+      addEventListeners();
 
-      fetch('https://api.github.com/users/' + userName)
-        .then((response)=> {response.json})
-        .then(function (body) {
-          self.profile = body;
-          self.update_profile();
-        })
+      Object.defineProperty(self, "username", {
+          get: function () {
+              const username = input.val();
+              const regexp = new RegExp(/^[a-zA-Z0-9-_]+$/);
 
-    })
+              if ('string' === typeof username && username.match(regexp)) {
+                  input.removeClass('.input--error');
 
-  }
+                  return username;
+              }
 
-  update_profile() {
-    $('#profile-name').text($('.username.input').val())
-    $('#profile-image').attr('src', this.profile.avatar_url)
-    $('#profile-url').attr('href', this.profile.html_url).text(this.profile.login)
-    $('#profile-bio').text(this.profile.bio || '(no information)')
+              input.addClass('input--error');
+
+              throw new Error('app.username is not valid');
+          },
+      });
+
+      function setDefaults() {
+          self.profile = {
+              bio: '',
+              avatar_url: '',
+              html_url: '',
+              login: '',
+          };
+      }
+
+      function addEventListeners() {
+          $('.load-username').on('click', function () {
+              fetch('https://api.github.com/users/' + self.username)
+                  .then(response => {
+                      if (200 === response.status) {
+                          return response.json()
+                      }
+
+                      if (404 === response.status) {
+                          input.addClass('input--error');
+                      }
+
+                      throw new Error('Could not fetch username. Error: ' + response.status);
+                  })
+                  .then(resp => updateGithubProfile(resp));
+
+              function updateGithubProfile(body) {
+                  self.profile = body;
+                  updateProfile();
+              }
+          });
+      }
+
+      function updateProfile() {
+          $('#profile-name').text(self.username);
+          $('#profile-image').attr('src', self.profile.avatar_url);
+          $('#profile-url').attr('href', self.profile.html_url).text(self.profile.login);
+          $('#profile-bio').text(self.profile.bio || '(no information)')
+      }
   }
 }
